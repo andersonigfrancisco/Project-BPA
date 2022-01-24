0ea9ad490badec765724b6cb74f107af66f841fa
andersonfranciscoig@gmail.com <andersonfranciscoig@gmail.com>
2022-01-24 14:19:23 +0100

    :hankey: add client

---
+++ Service-Front-End/src/Controllers/Client/Companies.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BPA.serviceapi.Models;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import java.util.Date;
+import javax.persistence.*;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+/**
+ *
+ * @author THL
+ */
+@Entity(name="companies")
+public class Companies {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    
+    @Column(nullable=false, length=150)
+    public String designation;
+    
+    @Column(nullable=false, length=2000)
+    public String description;
+    
+    
+    @NotFound(action = NotFoundAction.EXCEPTION)
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "Personid", referencedColumnName = "id",nullable = true)
+    public Person Person;
+   
+   
+    @Basic(optional = false)
+    @Column(insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdat;
+    
+    @Basic(optional = false)
+    @Column(insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedat;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Person getPerson() {
+        return Person;
+    }
+
+    public void setPerson(Person Person) {
+        this.Person = Person;
+    }
+
+    public Date getCreatedat() {
+        return createdat;
+    }
+
+    public void setCreatedat(Date createdat) {
+        this.createdat = createdat;
+    }
+
+    public Date getUpdatedat() {
+        return updatedat;
+    }
+
+    public void setUpdatedat(Date updatedat) {
+        this.updatedat = updatedat;
+    }
+   
+}
